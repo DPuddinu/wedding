dd2f869d7fb54a3d0bd31094057c6721729024bc
Dario <dario.puddinu@abinsula.com>
2022-03-15 17:15:15 +0100

    dependencies fix

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,7 +4,7 @@ import {MatDialogModule} from '@angular/material/dialog';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatListModule} from '@angular/material/list';
-
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   exports: [
@@ -13,6 +13,7 @@ import {MatListModule} from '@angular/material/list';
     MatInputModule,
     MatListModule,
     MatCheckboxModule,
+    MatButtonModule
   ]
 })
 export class MaterialExampleModule {}
